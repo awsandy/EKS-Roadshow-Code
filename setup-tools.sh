1ef4370840f7e9c2b018e7c72b82697f2221c3c3
awsandy <awsandy@amazon.com>
2022-07-08 17:31:53 +0100

    up

--- setup-tools.sh
+++ setup-tools.sh
@@ -71,7 +71,6 @@ echo "Setup helm"
 if [ ! `which helm 2> /dev/null` ]; then
   echo "Install helm"
   curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-  helm version --short
 fi
 
 
