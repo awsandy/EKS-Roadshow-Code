3aab4978a3121c19caa852a76e17298cb69c653d
awsandy <awsandy@amazon.com>
2022-07-08 19:43:14 +0100

    up

--- .automation/012-patching/01-patch.sh
+++ .automation/012-patching/01-patch.sh
@@ -7,5 +7,5 @@ eksctl utils update-coredns --cluster=eksworkshop-eksctl --approve
 kubectl get daemonset kube-proxy --namespace kube-system -o=jsonpath='{$.spec.template.spec.containers[:1].image}'
 kubectl describe deployment coredns --namespace kube-system | grep Image | cut -d "/" -f 3
 #
-eksctl upgrade nodegroup --name=nodegroup --cluster=eksworkshop-eksctl --kubernetes-version=1.21
+eksctl upgrade nodegroup --name=nodegroup --cluster=eksworkshop-eksctl --kubernetes-version=1.22
 kubectl get nodes --watch
