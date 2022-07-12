494ab5cd6c03bb1c52f24700c5ac852ad410a401
awsandy <awsandy@amazon.com>
2022-07-12 15:50:44 +0100

    up

--- setup-tools.sh
+++ setup-tools.sh
@@ -108,7 +108,5 @@ aws kms create-alias --alias-name alias/eksworkshop --target-key-id $(aws kms cr
 export MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyMetadata.Arn --output text)
 echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
 
-
-
 . ~/.bash_profile
 
